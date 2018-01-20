b978c4996eeb623d0896c9b587069a73cd2c51fa
Crisan G. Catalin <catacrisan_catacrsn@yahoo.com>
2018-01-21 00:28:49 +0200

    product_info: login user before add to cart

--- product_info.php
+++ product_info.php
@@ -4,7 +4,11 @@ require_once 'ProductDAO.php';
 require_once 'CartDAO.php';
 
 if (isset($_POST['add_product'])) {
-    CartDAO::addToCart($_SESSION['user_id'], $_GET['product_id'], 1);
+    if (isset($_SESSION['user_id'])) {
+        CartDAO::addToCart($_SESSION['user_id'], $_GET['product_id'], 1);
+    } else {
+        echo "Autentifica inainte de a adauga produse in cos!";
+    }
 }
 
 if (isset($_GET['product_id'])) {
@@ -17,6 +21,7 @@ if (isset($_GET['product_id'])) {
     }
     ?>
 
+    <a href='index.php'>Inapoi</a>
     <form action="product_info.php?product_id=<?php echo $_GET['product_id'] ?>" method="POST">
         <h1>Produs: <?php echo $response['name'] ?></h1>
         <h2>Pret: <?php echo $response['price'] ?>RON </h2>
@@ -24,9 +29,7 @@ if (isset($_GET['product_id'])) {
         <h2>Imagine: <?php echo $response['image'] ?> </h2>
         <input type="hidden" name="product_id" value="<?php echo $_GET['product_id'] ?>">
         <input type="submit" name="add_product" value="Adauga in cos">
-        <hr>
     </form>
-    <a href='index.php'>Inapoi</a>
 
     <?php
 } else {
@@ -34,4 +37,6 @@ if (isset($_GET['product_id'])) {
     echo "<a href='index.php'>Inapoi</a>";
     return;
 }
+
+require_once 'footer.php';
 ?>
