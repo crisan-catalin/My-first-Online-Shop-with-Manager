63bbdafd273213fcc7b67272f3c1d0be2604617a
Crisan G. Catalin <catacrisan_catacrsn@yahoo.com>
2018-01-21 00:28:09 +0200

    Login and Signup page: add header and footer

--- login.php
+++ login.php
@@ -1,5 +1,13 @@
-<form action="account_login.php" method="POST">
-	<input type="text" name="user_name" placeholder="Nume utilizator" pattern=".{5,}" required="required">
-	<input type="password" name="password" pattern=".{5,}" required="required">
-	<input type="submit" name="Login">
-</form>
+<?php
+require_once 'header.php';
+?>
+    <h1>Login</h1>
+    <form action="account_login.php" method="POST">
+        <input type="text" name="user_name" placeholder="Nume utilizator" pattern=".{5,}" required="required">
+        <input type="password" name="password" pattern=".{5,}" required="required">
+        <input type="submit" name="Login">
+    </form>
+
+<?php
+require_once 'footer.php';
+?>
--- signup.php
+++ signup.php
@@ -1,6 +1,16 @@
-<form action="account_signup.php" method="POST">
-	<input type="text" name="user_name" placeholder="Nume utilizator" pattern=".{6,}" required="required">
-	<input type="text" name="email" placeholder="Adresa email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$" required="required">
-	<input type="password" name="password" pattern=".{6,}" required="required">
-	<input type="submit" name="Login">
-</form>
+<?php
+require_once 'header.php';
+?>
+
+    <h1>Creaza cont</h1>
+    <form action="account_signup.php" method="POST">
+        <input type="text" name="user_name" placeholder="Nume utilizator" pattern=".{6,}" required="required">
+        <input type="text" name="email" placeholder="Adresa email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$"
+               required="required">
+        <input type="password" name="password" pattern=".{6,}" required="required">
+        <input type="submit" name="Login">
+    </form>
+
+<?php
+require_once 'footer.php';
+?>
